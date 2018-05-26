60108bb315945615189792010237f12dcc4fcb7a
Ferdi <ferdinandpretorius@gmail.com>
2018-05-27 00:54:54 +0100

    Add time and score props to stories

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,14 +3,16 @@ import { cleanUrl } from '../utils/index'
 
 import '../styles/story.css'
 
-function Story({ title, by, url }) {
+function Story({ title, by, url, time, score }) {
   return (
     <div className="story-card-wrapper">
       <a href={url} className="story-card">
         <div>
+          <div className="story-card-time">{time}</div>
           <div className="story-card-title">{title}</div>
           <div className="story-card-author">By {by}</div>
         </div>
+        <div className="story-card-score">{score}</div>
 
         <div className="story-card-link">{cleanUrl(url)}</div>
       </a>
