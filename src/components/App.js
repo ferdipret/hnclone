b54968471c43519962228a1a91f256d15b4ce02c
Ferdi <ferdinandpretorius@gmail.com>
2018-05-26 15:38:34 +0100

    Add some comment docs for `App` methods

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,27 @@ class App extends Component {
   constructor() {
     super()
 
+    /**
+     * All the app state is stored in the `App` container
+     *
+     * We'll manage state updates using a custom `store` function
+     * The store will use the same api as Redux
+     */
     this.state = store(undefined, {})
-  }
-
-  dispatch = action => {
-    action &&
-      action.type &&
-      this.setState(prevState => store(prevState, action))
   }
 
   render() {
     return <h1 className="app-title">App Component</h1>
+  }
+
+  /**
+   * Will dispatch actions to the store which will return our new state
+   * and set the state in `App`
+   */
+  dispatch = action => {
+    action &&
+      action.type &&
+      this.setState(prevState => store(prevState, action))
   }
 }
 
