8ff4f1d4122d812324f9ef1ed1280890eda98399
Ferdi <ferdinandpretorius@gmail.com>
2018-05-26 22:37:27 +0100

    Add some basic styles

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/story.css'
 
 function Story({ title, by, url }) {
   return (
-    <div>
-      <h4>{title}</h4>
+    <div className="story-card">
+      <h4 className="blue">{title}</h4>
       <div>
         <span>By</span> {by}
       </div>
