9ff9d372d7df6eca4331fe024560ca5f1695b2f2
Ferdi <ferdinandpretorius@gmail.com>
2018-05-27 01:08:23 +0100

    Add moment for time formatting

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { cleanUrl } from '../utils/index'
+import moment from 'moment'
 
 import '../styles/story.css'
 
 function Story({ title, by, url, time, score }) {
+  console.log(moment(time).format())
   return (
     <div className="story-card-wrapper">
       <a href={url} className="story-card">
         <div>
-          <div className="story-card-time">{time}</div>
+          <div className="story-card-time">
+            {moment.unix(time).format('DD MMMM YYYY')}
+          </div>
           <div className="story-card-title">{title}</div>
           <div className="story-card-author">By {by}</div>
         </div>
