d33bfd90cc0875aff9c1cc61c6e11012a85e59e9
Ferdi <ferdinandpretorius@gmail.com>
2018-05-28 14:14:39 +0100

    Added Header & header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import '../styles/header.css'
+
 function Header() {
   return (
     <div className="header">
-      <img src="hn-icon.png" alt="hacker news logo" />
+      <div className="img-wrapper">
+        <img src="hn-icon.png" alt="hacker news logo" />
+      </div>
+      <h1 className="title bg-color-2">HACKER NEWS</h1>
     </div>
   )
 }
